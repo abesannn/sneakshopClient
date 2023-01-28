d05d62db776014e43990a8c36f42c3588082f647
abeni <alvbesa@gmail.com>
2023-01-28 17:50:21 +0100

    logout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,16 @@ import { LoginComponent } from './component/shared/routed/login/login.component'
 import { UsuarioPlistAdminComponent } from './component/application/routed/admin/usuario-plist-admin/usuario-plist-admin.component';
 import { usuarioViewAdminRoutedComponent } from './component/application/routed/admin/usuario-view-admin-routed/usuario-view-admin-routed.component';
 import { UsuarioNewAdminComponent } from './component/application/routed/admin/usuario-new-admin/usuario-new-admin.component';
+import { LogoutComponent } from './component/shared/routed/logout/logout.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent, title: 'Login'},
   { path: 'admin/usuario/plist', component: UsuarioPlistAdminComponent, title: 'Plist usuarios'},
   { path: 'admin/usuario/view/:id', component: usuarioViewAdminRoutedComponent, title: 'Vista usuario'},
-  { path: 'admin/usuario/new', component: UsuarioNewAdminComponent, title: 'Nuevo usuario'}
-  
+  { path: 'admin/usuario/new', component: UsuarioNewAdminComponent, title: 'Nuevo usuario'},
+  { path: 'logout', component: LogoutComponent},
+
 ];
 
 @NgModule({
--- src/app/component/shared/routed/logout/logout.component.ts
+++ src/app/component/shared/routed/logout/logout.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IUsuario } from 'src/app/model/usuario-interface';
+import { MetadataService } from 'src/app/service/metadata.service';
+import { SessionService } from 'src/app/service/session.service';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
   styleUrls: ['./logout.component.css']
 })
-export class LogoutComponent {
+export class LogoutComponent  {
 
+  strOperation: string = "logout"
+  oUsuarioSession: IUsuario;
+
+  constructor(
+    private oRoute: ActivatedRoute,
+    private oRouter: Router,
+    private oSessionService: SessionService,
+    protected oLocation: Location,
+    public oMetadataService: MetadataService
+  ) {
+  }
+
+  public closeSession() {
+    this.oSessionService.logout().subscribe(data => {
+      localStorage.clear();
+      this.oRouter.navigate(['/', 'home'])
+    });
+  }
 }
